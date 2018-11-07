b0eb9017807cbfd0a000c2a8391510f77b4fbbfc
Waarum <merawataru@meryouwatarunomacbook-air.local>
2018-11-08 07:58:49 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 目良渉 on 2018/11/08.
+//  Copyright © 2018 Wataru Mera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
